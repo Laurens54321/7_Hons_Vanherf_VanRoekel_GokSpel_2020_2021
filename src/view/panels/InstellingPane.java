9ad812f80be0d13e948c4b770136c66c53a74e47
brick van roekel <brick.vanroekel@student.ucll.be>
2020-12-21 15:13:31 +0100

    InstellingPane

--- src/view/panels/InstellingPane.java
+++ src/view/panels/InstellingPane.java
@@ -2,21 +2,34 @@ package view.panels;
 
 import controller.InstellingController;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class InstellingPane extends GridPane {
+    VBox layout;
+    Label fileLabel;
     InstellingController instellingController;
 
     RadioButton excelFile;
     RadioButton textFile;
     Button saveButton;
 
+    public InstellingPane(){
+        layout = new VBox();
+        layout.setSpacing(10);
+
+        fileLabel.setText("Choose file type:");
+
+        HBox fileChoice = new HBox();
     public InstellingPane(InstellingController instellingController){
         this.instellingController = instellingController;
 
         ToggleGroup radioGroup = new ToggleGroup();
+
         excelFile = new RadioButton( "Data in Excel file");
         excelFile.setToggleGroup(radioGroup);
         excelFile.setOnAction(event -> setSaveLoadController("excel"));
@@ -25,9 +38,12 @@ public class InstellingPane extends GridPane {
         textFile.setToggleGroup(radioGroup);
         textFile.setOnAction(event -> setSaveLoadController("text"));
 
+        fileChoice.getChildren().addAll(excelFile,textFile);
+
         saveButton = new Button();
         saveButton.setText("Save");
 
+        layout.getChildren().addAll(fileLabel,fileChoice,saveButton);
         this.getChildren().addAll(excelFile, textFile, saveButton);
     }
 
