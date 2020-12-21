dd63e420d5cc928d207cd6d7e817a9bafed0a7a1
brick van roekel <brick.vanroekel@student.ucll.be>
2020-12-21 15:06:27 +0100

    InstellingPane

--- src/view/panels/InstellingPane.java
+++ src/view/panels/InstellingPane.java
@@ -1,10 +1,26 @@
 package view.panels;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 
 public class InstellingPane extends GridPane {
 
+    RadioButton excelFile;
+    RadioButton textFile;
+    Button saveButton;
+
     public InstellingPane(){
+        ToggleGroup radioGroup = new ToggleGroup();
+        excelFile = new RadioButton( "Data in Excel file");
+        excelFile.setToggleGroup(radioGroup);
+
+        textFile = new RadioButton("Data in tekstfile");
+        textFile.setToggleGroup(radioGroup);
+
+        saveButton.setText("Save");
+
 
     }
 }
